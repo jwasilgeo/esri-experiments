5049b71b8bb87df2a6416f313fe795aa9c81c7b2
Jacob Wasilkowski <jwasilkowski@esri.com>
2017-03-15 13:18:02 -0400

    toggling of color modes; cleaned up waiting on listeners

--- antipodes-2d/script.js
+++ antipodes-2d/script.js
@@ -8,23 +8,28 @@ require([
 ], function(
   FeatureLayer, Map, MapView, Viewpoint
 ) {
-  var credits = document.getElementById('credits');
   var antipodeInfo = document.getElementById('antipodeInfo');
+  var colorblindToggle = document.getElementById('colorblindToggle');
+  var credits = document.getElementById('credits');
 
-  // var longLat = [-4.484, 39.972];
-  var longLat = [-5.936, 39.296];
+  // MapView and Map initial values
+  var longLatTop = [-5.936, 39.296]; // top map view start location
+  var longLatAntipode = [180 + longLatTop[0], -longLatTop[1]]; // antipode map view start location
+  var scale = 30000000;
+  var countriesUrl = 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Countries_(Generalized)/FeatureServer/0';
+  var countriesOutFields = ['FID', 'Country'];
 
   var mapViewTop = new MapView({
     container: 'viewDivTop',
     map: new Map({
       basemap: 'dark-gray-vector',
       layers: [new FeatureLayer({
-        url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Countries_(Generalized)/FeatureServer/0',
-        outFields: ['FID', 'Country']
+        url: countriesUrl,
+        outFields: countriesOutFields
       })]
     }),
-    center: longLat,
-    scale: 30000000,
+    center: longLatTop,
+    scale: scale,
     rotation: 0,
     ui: {
       components: ['zoom', 'compass', 'attribution']
@@ -37,13 +42,14 @@ require([
   var mapViewAntipode = new MapView({
     container: 'viewDivAntipode',
     map: new Map({
+      basemap: 'dark-gray-vector',
       layers: [new FeatureLayer({
-        url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Countries_(Generalized)/FeatureServer/0',
-        outFields: ['FID', 'Country']
+        url: countriesUrl,
+        outFields: countriesOutFields
       })]
     }),
-    center: [180 + longLat[0], -longLat[1]], // antipode start location
-    scale: 30000000,
+    center: longLatAntipode,
+    scale: scale,
     rotation: 180,
     ui: {
       components: []
@@ -53,79 +59,163 @@ require([
     }
   });
 
-  mapViewTop.then(function() {
-    mapViewTop.ui.add('credits', 'bottom-right');
-    credits.style.display = 'flex';
+  // do a bunch of waiting on view and layer resources to be loaded/ready
+  mapViewTop
+    .then(function() {
+      mapViewTop.ui.add('antipodeInfo', 'top-right');
+      antipodeInfo.style.display = 'flex';
 
-    mapViewTop.ui.add('antipodeInfo', 'top-right');
-    antipodeInfo.style.display = 'flex';
+      mapViewTop.ui.add('colorblindToggle', 'top-left');
+      colorblindToggle.style.display = 'flex';
+      colorblindToggle.addEventListener('click', changeColorScheme);
+      // set the intial "traditional" color scheme
+      changeColorScheme();
 
-    mapViewAntipode.then(function() {
-      // override symbology
-      var mapViewTopCountryLayer = mapViewTop.map.layers.items[0];
-      // for dark blue mode...
-      //mapViewTopCountryLayer.renderer.symbol.color = [57, 87, 114, 1];
-      //mapViewTopCountryLayer.renderer.symbol.outline.color = [37, 50, 69, 1];
-      // for traditional mode...
-      mapViewTopCountryLayer.renderer.symbol.color = [113, 170, 113, 0.65];
-      mapViewTopCountryLayer.renderer.symbol.outline.color = [167, 226, 242, 0.5];
-      //for grayscale mode
-      // mapViewTopCountryLayer.renderer.symbol.color = [79, 79, 79, 1];
-      // mapViewTopCountryLayer.renderer.symbol.outline.color = [22, 22, 22, 1];
-      // for bright mode...
-      // mapViewTopCountryLayer.renderer.symbol.color = [0, 153, 255, 1];
-      // mapViewTopCountryLayer.renderer.symbol.outline.color = [244, 244, 244, 1];
+      mapViewTop.ui.add('credits', 'bottom-right');
+      credits.style.display = 'flex';
+
+      // establish listener to set antipode map view position and rotation
+      // as a reaction to changes in the top map view position
+      mapViewTop.watch('viewpoint', moveAntipodeMapView);
+
+      return mapViewTop.map.layers.getItemAt(0).load();
+    })
+    .then(function() {
+      return mapViewAntipode.map.layers.getItemAt(0).load();
+    })
+    .then(function(antipodeCountryLayer) {
+      return mapViewAntipode.whenLayerView(antipodeCountryLayer);
+    })
+    .then(function(antipodeCountryLayerView) {
+      // the basemaps were just used to help with map initialization;
+      // get rid of them for the 2 MapViews
       mapViewTop.map.basemap = null;
+      mapViewAntipode.map.basemap = null;
 
-      var mapViewAntipodeCountryLayer = mapViewAntipode.map.layers.items[0];
-      // for traditional AND dark blue modes. Turn up opacity for dark blue modes to ~0.3 for fill and 0.5 for outline ...
-      mapViewAntipodeCountryLayer.renderer.symbol.color = [255, 0, 0, 0.2];
-      mapViewAntipodeCountryLayer.renderer.symbol.outline.color = [200, 0, 0, 0.3];
-      // for grayscale mode...
-      // mapViewAntipodeCountryLayer.renderer.symbol.color = [48, 48, 48, 1];
-      // mapViewAntipodeCountryLayer.renderer.symbol.outline.color = [249, 78, 78, 1];
-      // for bright mode...
-      // mapViewAntipodeCountryLayer.renderer.symbol.color = [204, 232, 37, 1];
-      // mapViewAntipodeCountryLayer.renderer.symbol.outline.color = [0, 0, 153, 1];
-
-      // moveToAntipode().then(function() {
-      //   mapViewTop.on('pointer-move', function(evt) {
-      //     mapViewTop.hitTest({
-      //       x: evt.x,
-      //       y: evt.y
-      //     }).then(function(response) {
-      //       antipodeInfo.children[0].innerText = 'ANTIPODE';
-      //       if (response.results.length && response.results[0].graphic) {
-      //         antipodeInfo.children[2].innerText = response.results[0].graphic.attributes.Country.toUpperCase();
-      //       } else {
-      //         antipodeInfo.children[2].innerText = 'OCEAN';
-      //       }
-      //     });
-      //
-      //     mapViewAntipode.hitTest({
-      //       x: evt.x,
-      //       y: evt.y
-      //     }).then(function(response) {
-      //       if (response.results.length && response.results[0].graphic) {
-      //         console.log('antipode: ', response.results[0].graphic.attributes.Country)
-      //       }
-      //     });
-      //   });
-      // });
-
-      mapViewTop.watch('viewpoint', moveToAntipode);
-
-      function moveToAntipode() {
-        return mapViewAntipode.goTo({
-          target: new Viewpoint({
-            rotation: 180 - mapViewTop.rotation,
-          }),
-          center: [180 + mapViewTop.center.longitude, -mapViewTop.center.latitude],
-          scale: mapViewTop.scale
-        }, {
-          animate: false
-        });
-      }
+      // wait a bit more just to be safe
+      setTimeout(function() {
+        establishCountryInteractionListener(mapViewTop, antipodeCountryLayerView);
+      }, 5000);
     });
-  });
+
+  function moveAntipodeMapView() {
+    return mapViewAntipode.goTo({
+      target: new Viewpoint({
+        rotation: 180 - mapViewTop.rotation,
+      }),
+      center: [180 + mapViewTop.center.longitude, -mapViewTop.center.latitude],
+      scale: mapViewTop.scale
+    }, {
+      animate: false
+    });
+  }
+
+  function establishCountryInteractionListener(mapViewTop, antipodeCountryLayerView) {
+    mapViewTop.on('pointer-move', function(evt) {
+      // find out the country for the top MapView
+      mapViewTop.hitTest({
+        x: evt.x,
+        y: evt.y
+      }).then(function(response) {
+        var topCountry = 'OCEAN'; // default value if there is no country under the mouse
+        if (response.results.length && response.results[0].graphic) {
+          topCountry = response.results[0].graphic.attributes.Country.toUpperCase();
+        }
+        antipodeInfo.children[0].innerText = topCountry;
+      });
+
+      // find out the country for the antipode MapView,
+      // using client-side graphics in the FeatureLayerView
+      var antipodePoint = mapViewTop.toMap({
+        x: evt.x,
+        y: evt.y
+      });
+      antipodePoint.longitude += 180;
+      antipodePoint.latitude *= -1;
+      antipodePoint.normalize();
+
+      var antipodeCountry = 'OCEAN'; // default value if there is no country under the mouse
+      antipodeCountryLayerView.featuresView.graphics.some(function(graphic) {
+        // point-in-polygon check with client-side country geometries
+        if (graphic.geometry.contains(antipodePoint)) {
+          antipodeCountry = graphic.attributes.Country.toUpperCase();
+          return true;
+        } else {
+          return false;
+        }
+      });
+      antipodeInfo.children[2].innerText = antipodeCountry;
+    });
+  }
+
+  var nextColorScheme = 'traditional';
+
+  function changeColorScheme() {
+    var topCountryFillColor,
+      topCountryOutlineColor,
+      mixBlendMode,
+      antipodeCountryFillColor,
+      antipodeCountryOutlineColor,
+      backgroundColor,
+      topCountryTextColor,
+      antipodeCountryTextColor;
+
+    if (nextColorScheme === 'traditional') {
+      nextColorScheme = 'grayscale';
+
+      topCountryFillColor = [113, 170, 113, 0.65];
+      topCountryOutlineColor = [167, 226, 242, 0.5];
+      mixBlendMode = 'normal'; // or 'luminosity'
+      antipodeCountryFillColor = [255, 0, 0, 0.2];
+      antipodeCountryOutlineColor = [200, 0, 0, 0.3];
+      backgroundColor = '#a7e2f2';
+      topCountryTextColor = '#93d893';
+      antipodeCountryTextColor = 'rgb(177, 34, 34)';
+    } else if (nextColorScheme === 'grayscale') {
+      nextColorScheme = 'bright';
+
+      topCountryFillColor = [79, 79, 79, 1];
+      topCountryOutlineColor = [22, 22, 22, 1];
+      mixBlendMode = 'lighten'; // or 'luminosity', 'exclusion'
+      antipodeCountryFillColor = [48, 48, 48, 1];
+      antipodeCountryOutlineColor = [249, 78, 78, 1];
+      backgroundColor = '#161616';
+      topCountryTextColor = '#93d893';
+      antipodeCountryTextColor = 'rgb(177, 34, 34)';
+    } else if (nextColorScheme === 'bright') {
+      nextColorScheme = 'traditional';
+
+      topCountryFillColor = [0, 153, 255, 1];
+      topCountryOutlineColor = [244, 244, 244, 1];
+      mixBlendMode = 'multiply';
+      antipodeCountryFillColor = [204, 232, 37, 1];
+      antipodeCountryOutlineColor = [0, 0, 153, 1];
+      backgroundColor = '#f4f4f4';
+      topCountryTextColor = '#93d893';
+      antipodeCountryTextColor = 'rgb(177, 34, 34)';
+    }
+
+    var topCountryLayer = mapViewTop.map.layers.getItemAt(0);
+    topCountryLayer.renderer.symbol.color = topCountryFillColor;
+    topCountryLayer.renderer.symbol.outline.color = topCountryOutlineColor;
+    mapViewTop.container.style.mixBlendMode = mixBlendMode;
+
+    var antipodeCountryLayer = mapViewAntipode.map.layers.getItemAt(0);
+    antipodeCountryLayer.renderer.symbol.color = antipodeCountryFillColor;
+    antipodeCountryLayer.renderer.symbol.outline.color = antipodeCountryOutlineColor;
+    mapViewAntipode.container.style.backgroundColor = backgroundColor;
+
+    antipodeInfo.children[0].style.color = topCountryTextColor;
+    antipodeInfo.children[2].style.color = antipodeCountryTextColor;
+
+    // force a refresh (this goTo shouldn't be noticed by the user)
+    mapViewTop.goTo({
+      target: new Viewpoint({
+        rotation: 360 + mapViewTop.rotation,
+      })
+    }, {
+      animate: false
+    });
+  }
+
 });
