b1a4fc291bd41b561af2f9f9cebf5f1661629c26
jwasilgeo <jwasilkowski@esri.com>
2021-02-01 15:56:09 -0600

    updated antipodes and custom-contours demos

--- antipodes/script.js
+++ antipodes/script.js
@@ -1,12 +1,4 @@
-(function() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile/i.test(navigator.userAgent)) {
-    document.querySelector('#mainContent').style.display = 'none';
-    document.querySelector('#errorMessage').style.display = 'block';
-    document.querySelector('#errorMessage img').src = './antipodes.gif';
-    document.body.style.setProperty('overflow', 'auto');
-    return;
-  }
-
+(function () {
   require([
     'esri/Graphic',
     'esri/layers/GraphicsLayer',
@@ -14,7 +6,7 @@
     'esri/views/SceneView',
 
     'dojo/domReady!'
-  ], function(
+  ], function (
     Graphic, GraphicsLayer, Map, SceneView
   ) {
     document.querySelector('#mainContent').style.display = 'inline';
@@ -58,19 +50,19 @@
       }
     });
 
-    sceneViewB.on('click,double-click,drag,hold,key-down,key-up,mouse-wheel,pointer-down,pointer-move,pointer-up', function(evt) {
+    sceneViewB.on('click,double-click,drag,hold,key-down,key-up,mouse-wheel,pointer-down,pointer-move,pointer-up', function (evt) {
       evt.stopPropagation();
     });
 
-    sceneViewA.when(function() {
+    sceneViewA.when(function () {
       // start the views zoomed in, and then zoom out to show the globe
-      setTimeout(function() {
+      setTimeout(function () {
         sceneViewA.goTo({
           zoom: 3
         });
       }, 10000);
 
-      sceneViewA.watch('camera', function(sceneViewACamera) {
+      sceneViewA.watch('camera', function (sceneViewACamera) {
         // set the antipode camera in the secondary scene view
         var antipodeCamera = sceneViewACamera.clone();
         antipodeCamera.position.longitude += 180;
@@ -78,58 +70,60 @@
         sceneViewB.camera = antipodeCamera;
       });
 
-      sceneViewA.on('pointer-move', function(evt) {
-        var pointerGeometry = sceneViewA.toMap({
-          x: evt.x,
-          y: evt.y
+      sceneViewA.on('click,pointer-move', updateGraphicsAndElevations);
+    });
+
+    function updateGraphicsAndElevations (evt) {
+      var pointerGeometry = sceneViewA.toMap({
+        x: evt.x,
+        y: evt.y
+      });
+
+      if (pointerGeometry) {
+        var pointerGraphic = new Graphic({
+          geometry: pointerGeometry,
+          symbol: {
+            type: 'simple-marker',
+            color: '#f44336',
+            outline: {
+              color: 'orange',
+              width: 1.75
+            },
+            size: 17
+          }
         });
 
-        if (pointerGeometry) {
-          var pointerGraphic = new Graphic({
-            geometry: pointerGeometry,
-            symbol: {
-              type: 'simple-marker',
-              color: '#f44336',
-              outline: {
-                color: 'orange',
-                width: 1.75
-              },
-              size: 17
-            }
-          });
+        var antipodeGraphic1 = pointerGraphic.clone();
+        antipodeGraphic1.geometry.longitude += 180;
+        antipodeGraphic1.geometry.latitude *= -1;
 
-          var antipodeGraphic1 = pointerGraphic.clone();
-          antipodeGraphic1.geometry.longitude += 180;
-          antipodeGraphic1.geometry.latitude *= -1;
+        // another antipode (really in the same place) to avoid "missing" graphic wrap-around
+        var antipodeGraphic2 = pointerGraphic.clone();
+        antipodeGraphic2.geometry.longitude -= 180;
+        antipodeGraphic2.geometry.latitude *= -1;
 
-          // another antipode (really in the same place) to avoid "missing" graphic wrap-around
-          var antipodeGraphic2 = pointerGraphic.clone();
-          antipodeGraphic2.geometry.longitude -= 180;
-          antipodeGraphic2.geometry.latitude *= -1;
+        graphicsLayer.removeAll();
+        graphicsLayer.addMany([pointerGraphic, antipodeGraphic1, antipodeGraphic2]);
 
-          graphicsLayer.removeAll();
-          graphicsLayer.addMany([pointerGraphic, antipodeGraphic1, antipodeGraphic2]);
+        // display both views' altitude values
+        var altitudeAString = pointerGeometry.z >= 0 ? pointerGeometry.z.toFixed(0) : 0;
+        altitudeANode.innerHTML = altitudeAString + 'm';
+        altitudeANode.style.color = getAltitudeColor(altitudeAString);
 
-          // display both views' altitude values
-          var altitudeAString = pointerGeometry.z >= 0 ? pointerGeometry.z.toFixed(0) : 0;
-          altitudeANode.innerHTML = altitudeAString + 'm';
-          altitudeANode.style.color = getAltitudeColor(altitudeAString);
+        // do a map --> screen --> map conversion to forcibly update the antipode point geometry to have the correct "z" altitude value
+        var antipodePointForAltitude = sceneViewB.toScreen(antipodeGraphic1.geometry);
+        antipodePointForAltitude = sceneViewB.toMap({
+          x: antipodePointForAltitude.x,
+          y: antipodePointForAltitude.y
+        });
 
-          // do a map --> screen --> map conversion to forcibly update the antipode point geometry to have the correct "z" altitude value
-          var antipodePointForAltitude = sceneViewB.toScreen(antipodeGraphic1.geometry);
-          antipodePointForAltitude = sceneViewB.toMap({
-            x: antipodePointForAltitude.x,
-            y: antipodePointForAltitude.y
-          });
-
-          var altitudeBString = antipodePointForAltitude && antipodePointForAltitude.z >= 0 ? antipodePointForAltitude.z.toFixed(0) : 0;
-          altitudeBNode.innerHTML = altitudeBString + 'm';
-          altitudeBNode.style.color = getAltitudeColor(altitudeBString);
-        } else {
-          clearAltitudes();
-        }
-      });
-    });
+        var altitudeBString = antipodePointForAltitude && antipodePointForAltitude.z >= 0 ? antipodePointForAltitude.z.toFixed(0) : 0;
+        altitudeBNode.innerHTML = altitudeBString + 'm';
+        altitudeBNode.style.color = getAltitudeColor(altitudeBString);
+      } else {
+        clearAltitudes();
+      }
+    }
 
     function clearAltitudes() {
       altitudeANode.innerHTML = '---';
